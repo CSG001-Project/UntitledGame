d1194e5ba6dbce9b268da87018fe5bea8312bc9b
p2949 <p2949@skynet.ie>
2025-02-08 18:54:18 +0000

    another fail... missed a few files on the last one

--- scripts/characters/player_movement.cs
+++ scripts/characters/player_movement.cs
@@ -1,27 +1,53 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
-public partial class player_movement : CharacterBody2D
+public partial class player_movement : StaticBody2D
 {
+	[Export]
+	public int TileSize { get; set; } = 64;
 	bool has_moved = false;
-	public override void _PhysicsProcess(double delta)
+	
+	
+	public Vector2 GetInput()
 	{
-		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		if (direction != Vector2.Zero && has_moved == false)
-		{
-			Position += 32 * direction;
-			has_moved = true;
-		}
+
+	   Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+	   return direction;
+		
+	}
+	public async void Move(Vector2 direction) { 
 		if (has_moved == true)
 		{
-			_Ready();
+				DelayMethod();
 		}
+		else if (direction != Vector2.Zero && has_moved == false)
+		{
+			direction = direction.Normalized();
+			Position += TileSize * direction;
+			Position = Position.Snapped(Vector2.One * TileSize);
+			has_moved = true;
+		}
+	}
+	private async void DelayMethod()
+{
+	var timer = GetTree().CreateTimer(4f);
+	await Task.Delay(TimeSpan.FromMilliseconds(1));
+	timer.Timeout += OnTimerTimeout;
+}
+
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 direction = GetInput();
+		Move(direction);
+		//MoveAndSlide();
 	}
 	
 	public override void _Ready()
 	{
-		var timer = GetTree().CreateTimer(1f);
-		timer.Timeout += OnTimerTimeout;
+		Position = Position.Snapped(Vector2.One * TileSize);
+		Position += Vector2.One * TileSize;
 	}
 	private void OnTimerTimeout()
 	{
