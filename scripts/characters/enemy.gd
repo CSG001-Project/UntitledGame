4405bd934816d7a5ab478ea9cfbd3826388501c5
Mykhailo Fedenko <108079572+Absolutely0Perfect@users.noreply.github.com>
2025-02-23 14:09:17 +0000

    Enemies attack(fr)
    It prints "attack" so Its emotional type of damage

--- scripts/characters/enemy.gd
+++ scripts/characters/enemy.gd
@@ -3,6 +3,8 @@ extends StaticBody2D
 @onready var sprite = $Sprite2D
 @onready var health = $Health
 
+@export var weapon: Node2D
+
 func _ready() -> void:
 	# Update sprite position, add self to the list of enemies that can take turns
 	sprite.global_position = global_position
@@ -24,6 +26,9 @@ func turn() -> void:
 	var movement: Vector2 = Vector2(cos(angle), sin(angle))
 	if !test_move(transform, movement * 32):
 		position += movement * 32
+	elif (global_position.x == player_position.x && abs(global_position.y - player_position.y) <= 40) || (global_position.y == player_position.y && abs(global_position.x - player_position.x) <= 40):
+		#attack animation and actuasl attack here
+		print("attack")
 
 func update_sprite() -> void:
 	# Tween sprite to new position after turn is finished
