2104bdcc30eb34c226e2e8c73660ee0793ee2657
Mykhailo Fedenko <108079572+Absolutely0Perfect@users.noreply.github.com>
2025-03-02 16:05:26 +0000

    Gays I fcked Up
    Please fix it, i dont know what I am doing at this point

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -3,7 +3,7 @@ extends BaseCharacter
 @onready var timer = $Timer
 @onready var sprite = $Sprite
 @onready var arrow = $Sprite/Arrow
-@export var weapon: Node2D
+@export var weapon: PackedScene
 
 const TILE_SIZE: int = 32
 
@@ -21,7 +21,11 @@ func _physics_process(_delta: float) -> void:
 	
 	sprite.play("idle_"+direction)
 	
-	if Input.is_action_just_released("attack"):
+	if Input.is_action_just_released("attack") && weapon.is_ranged:
+		if try_shooting():
+			await shoot()
+			TurnManager.enemy_turn()
+	elif Input.is_action_just_released("attack"):
 		# Wait for the attack to finish and then call the enemy turn
 		update_direction(sin(angle))
 		await weapon.attack()
@@ -55,3 +59,34 @@ func update_sprite() -> void:
 	# Tween the sprite to the new position when the turn is finished
 	var tween = get_tree().create_tween()
 	tween.tween_property(sprite, "global_position", global_position, 0.1)
+
+func try_shooting() -> bool:
+	var mouse_position: Vector2 = get_global_mouse_position()
+	var cool_name_for_a_variable: Vector2 = global_position - mouse_position
+	var sum_of_boolshit: int = snappedi(abs(cool_name_for_a_variable.x) + abs(cool_name_for_a_variable.y), 32);
+	if not sum_of_boolshit <= weapon.range * TILE_SIZE:
+		return false
+	else:
+		if check_collisions(global_position, mouse_position):
+			return false
+		else:
+			return true
+
+func check_collisions(start: Vector2, end: Vector2) -> bool:
+	var space_state = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(start, end)
+	var result = space_state.intersect_ray(query)
+	
+	if not result.is_empty():
+		return true
+	return false
+
+func shoot():
+	var weapon_instance = weapon.instantiate()
+	var mouse_position: Vector2 = get_global_mouse_position()
+	mouse_position.x = snappedi(mouse_position.x, 32)
+	mouse_position.y = snappedi(mouse_position.y, 32)
+	weapon_instance.global_rotation = (global_position - mouse_position).angle()
+	
+	while not weapon_instance.global_position.x.is_equal_approx(mouse_position.x) and weapon_instance.global_position.y.is_equal_approx(mouse_position.y):
+		weapon_instance.position = weapon_instance.position.move_toward(mouse_position, 1)
--- scripts/global/turn_manager.gd
+++ scripts/global/turn_manager.gd
@@ -1,5 +1,5 @@
 extends Node
-
+class_name Ranged
 var enemies = []
 var player_turn = true
 
