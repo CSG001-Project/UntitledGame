739026d3d11270a66518c83f52c186f14b18534c
piraker-grinor <sparkslizer@gmail.com>
2025-04-23 12:16:51 +0100

    health unlobotomied

--- scripts/ui/health_bar.gd
+++ scripts/ui/health_bar.gd
@@ -6,4 +6,4 @@ func set_health_bar(current: int, max_health: int) -> void:
 	await get_tree().process_frame
 	self.max_value = max_health
 	self.value = current
-	health_label.text = "Health: %d/%d" % [max_health, current]
+	health_label.text = "Health: %d/%d" % [current, max_health]
